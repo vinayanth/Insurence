b9384b4e849f4bedae0cd1879c9dff583c3d3a71
vinayanth <vinayanth@gmail.com>
2022-09-16 12:03:22 +0530

    addeda new file

---
+++ main.py
@@ -0,0 +1,5 @@
+def report_main():
+    print("hey i am in main folder i am new")
+
+def report_demo():
+    print("i am the 2nd pgm in main")
---
+++ subpgm/sub.py
@@ -0,0 +1,5 @@
+def report_sub():
+    print("hey i am inside sub folder")
+
+def report_demo1():
+    print("HI i am 2nd pgn inside sub folder")
